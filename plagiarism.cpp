8af540541b59f74e0c8c13fe21fdfd91373be48f
Yauheni Ramankou <iromankov2010@gmail.com>
2021-10-15 19:13:56 +0300

    init: start anti plagiarism project

---
+++ plagiarism.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+double antiPlagiarism (string text, string fragment);
+
+int main()
+{
+  string text = "\0";
+  string fragment = "\0";
+
+  antiPlagiarism(text, fragment);
+
+  return 0;
+}
+
+double antiPlagiarism (string text, string fragment)
+{
+  double result = 0.0;
+
+  cout << "In progress.." << endl;
+
+  return result;
+}
